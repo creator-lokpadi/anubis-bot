abb61082eae41535e0f5fa8c19e8a3af263e45e0
anubiskun <adivian24@gmail.com>
2022-01-16 17:55:14 +0700

    fix badword biar owner ngomong kasar ga di bales sama bot :v

--- plugins/_antibadword.js
+++ plugins/_antibadword.js
@@ -1,14 +1,15 @@
 /*
 By : Anubiskun
 */
-let rgx = /(bot|adi )?(asu|b(an)?gsa?t|jemb(aw)?ut|ko?nt(o|i)?l|memek|peler|pejuh|janc(u|o)k|bawuk|silah|cingireh|anj(ing|ir|ey|ay|rot)|kirik|celek|ndas|babi|tol(|io)l)/i
+let linkRegex = /(asu|b(an)?gsa?(d|t)|jemb(aw)?ut|ko?nt(o|i)?l|memek|peler|pejuh|janc(u|o)k|bawuk|silah|cingireh|anj(ing|ir|ey|ay|rot)|kirik|celek|ndas|babi|tol(|io)l)/i
 module.exports = {
-    async all(m) {
+    async all(m, { isOwner, isBotAdmin }) {
     if (m.isBaileys && m.fromMe) return true
     let chat = global.db.data.chats[m.chat]
-    let isBdwrd = rgx.exec(m.text)
+    let isBdwrd = linkRegex.exec(m.text)
 
     if (chat.bdwrd && isBdwrd) {
+      if (!isOwner) return true
       m.reply('kasar kimak!')
     }
     return true
